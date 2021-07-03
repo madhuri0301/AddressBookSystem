d29b8ffb9802b825ace054e102272f452a7df3f8
Madhuri <harshupatil0301@gmail>
2021-07-03 19:46:00 +0530

    [Madhuri]Add : Creating Contacts In Address Book

---
+++ AddressBookSystem/ContactDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class ContactDetails
+    {
+        // Creating setter and getter for each property  
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+        public long phoneNumber { get; set; }
+        public string emailId { get; set; }
+
+        public ContactDetails()
+        {
+
+        }
+        public ContactDetails(string firstName, string lastName, string address, string city, string state, int zipCode, long phoneNumber, string emailId)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.emailId = emailId;
+
+        }
+        public void display()
+        {
+            Console.WriteLine(firstName + " " + lastName + " " + address + " " + city + " " + state + " " + zipCode + " " + phoneNumber + " " + emailId);
+        }
+    }
+
+}
+
