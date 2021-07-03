9dd6d537ac88e836fcf6b16c164d758818fd716c
Madhuri <harshupatil0301@gmail>
2021-07-03 20:40:39 +0530

    [Madhuri]Add : Editing A Contact From Address Book

--- AddressBookSystem/ContactDetails.cs
+++ AddressBookSystem/ContactDetails.cs
@@ -78,5 +78,89 @@ namespace AddressBookSystem
 
             }
         }
+        public void EditContact()
+        {
+            Console.WriteLine("Enter the first name of the person: ");
+            string fName = Console.ReadLine();
+            foreach (ContactDetails c in contacts)
+            {
+                if (c.firstName.Equals(fName))
+                {
+                    int editChoice = 0;
+                    while (editChoice != 9)
+                    {
+                        Console.WriteLine("Enter the following choice");
+                        Console.WriteLine("1. Edit First Name");
+                        Console.WriteLine("2. Edit Last Name");
+                        Console.WriteLine("3. Edit Address");
+                        Console.WriteLine("4. Edit City");
+                        Console.WriteLine("5. Edit State");
+                        Console.WriteLine("6. Edit Zip");
+                        Console.WriteLine("7. Edit Phone Number");
+                        Console.WriteLine("8. Edit E-mail");
+                        Console.WriteLine("9. Exit");
+                        Console.WriteLine("Enter your choice: ");
+                        editChoice = Convert.ToInt32(Console.ReadLine());
+
+                        switch (editChoice)
+                        {
+                            case 1:
+                                Console.WriteLine("1. Edit First Name");
+                                string fname = Console.ReadLine();
+                                c.firstName = fname;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 2:
+                                Console.WriteLine("1. Edit Last Name");
+                                string lname = Console.ReadLine();
+                                c.lastName = lname;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 3:
+                                Console.WriteLine("1. Edit Address Name");
+                                string aAddress = Console.ReadLine();
+                                c.address = aAddress;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 4:
+                                Console.WriteLine("1. Edit City Name");
+                                string cCity = Console.ReadLine();
+                                c.city = cCity;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 5:
+                                Console.WriteLine("1. Edit State");
+                                string sState = Console.ReadLine();
+                                c.state = sState;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 6:
+                                Console.WriteLine("1. Edit Zip");
+                                int zCode = Convert.ToInt32(Console.ReadLine());
+                                c.zipCode = zCode;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 7:
+                                Console.WriteLine("1. Edit Phone Number");
+                                long phNum = Convert.ToInt64(Console.ReadLine());
+                                c.phoneNumber = phNum;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                            case 8:
+                                Console.WriteLine("1. Edit Email");
+                                string mail = Console.ReadLine();
+                                c.emailId = mail;
+                                Console.WriteLine("Edited Successfully");
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid name");
+                }
+
+            }
+        }
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -13,11 +13,33 @@ namespace AddressBookSystem
             List<ContactDetails> contacts = new List<ContactDetails>();
             ContactDetails contact = new ContactDetails();
 
-            //Add New Contact
-            contact.AddNewContact();
-
-            //Display entered contacts
-            contact.DisplayContact();
+            bool flag = true;
+            int choice;
+            while (flag)
+            {
+                Console.WriteLine("\n1. Display All Contacts\n2. Add New Contact\n3. Edit a Contact\n4. Exit");
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    contact.DisplayContact();
+                }
+                else if (choice == 2)
+                {
+                    contact.AddNewContact();
+                }
+                else if (choice == 3)
+                {
+                    contact.EditContact();
+                }
+                else if (choice == 4)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
         }
     }
 }
